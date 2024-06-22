0b142d8939c60f6e2a36e4fd63597106d7453315
pcm9909 <pcm9909@naver.com>
2024-06-23 03:10:24 +0900

    [Chore] free함수 추가

--- main.c
+++ main.c
@@ -1,4 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chunpark <chunpark@student.42gyeongsan.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/22 19:17:07 by chunpark          #+#    #+#             */
+/*   Updated: 2024/06/22 20:25:25 by chunpark         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philosopher.h"
+
+void	free_resource(t_resource *resource)
+{
+	free(resource->philos);
+	sem_close(resource->eat);
+	sem_close(resource->write);
+	sem_close(resource->forks);
+	sem_close(resource->cmp_finish);
+}
 
 int	main(int argc, char **argv)
 {
